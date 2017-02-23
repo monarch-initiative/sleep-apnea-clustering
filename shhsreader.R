7c8e7c94dec7ac63cb8f93df7c2a86fa4ce2590b
Jason Li <lja@ohsu.edu>
2017-02-22 19:00:17 -0800

    just a notebook for trying new things

--- shhsreader.R
+++ shhsreader.R
@@ -2,10 +2,13 @@ library(tidyverse)
 
 setwd("~/Documents/MSthesis/datasets")
 shhs2<-read_csv("shhs2-dataset-0.11.0.csv")
+shhs1<-read_csv("shhs1-dataset-0.11.0.csv")
 datadict<-read_csv("shhs-data-dictionary-0.11.0-variables.csv")
 
+shhsWhole<-left_join(shhs1, shhs2, by="obf_pptid")
+
 Admin<-datadict %>%
-  filter(folder=="Administrative")
+  filter(str_detect(folder, "Administrative"))
 
 shhs2 %>%
   select(sh322a,sh323a)
@@ -14,3 +17,77 @@ ids<-Admin %>%
   select(id)
 shhs2 %>%
   select(shhs2_hi, shhs2_ms)
+
+ids.df<-as.data.frame(ids)
+
+shhs2 %>%
+  dplyr::select(starts_with("shhs2"))
+
+interim.data<-read_csv("shhs-interim-followup-dataset-0.11.0.csv")
+interim.data %>%
+  dplyr::select(accidents)
+
+
+cols_admin<-Admin %>% select (id) %>% collect %>% .[["id"]]
+shhsWhole %>%
+  dplyr::select(one_of(my_cols[2:25]))
+
+#alternative way
+iris2 %>% select(Species) %>% unlist(use.names = FALSE)
+
+#another alternative
+pull <- function(x,y) {x[,if(is.name(substitute(y))) deparse(substitute(y)) else y, drop = FALSE][[1]]}
+
+pull (Admin, id)
+library(stringr)
+datadict %>%
+  filter(str_detect(folder, "Heart Rate"))
+
+percent3<-datadict %>%
+  filter(str_detect(display_name, "3%"))
+percent3ColNames<-percent3 %>% select(id) %>% unlist(use.names = FALSE)
+shhs2%>%select (-(one_of(percent3ColNames)))
+
+shhs2 %>% select(-ecgdate)
+
+colsToLower <- function(df) {
+  names(df) <- tolower(names(df))
+  df
+}
+
+mtcars %>% upcase %>% select(MPG)
+
+assertthat::has_name(shhs1, "shhs1_bp")
+colnames(shhs1)
+
+filter(Admin, assertthat::has_name(shhs1, cols_admin))
+cols_admin.shhs1<-Admin %>%
+  filter(assertthat::has_name(shhs1, cols_admin)) %>%
+  select (id) %>% 
+  collect %>% 
+  .[["id"]]
+
+shhs1.noadmin<-shhs1 %>%
+  select(-one_of(cols_admin.shhs1))
+
+sq<-datadict %>%
+  filter(str_detect(folder, "Signal Quality"))
+
+miscToRemove<-datadict %>%
+  filter(id, dplyr::starts_with("pm"))
+
+drop.cols<-c("pm217", "pm227", "ecgdate")
+shhs2 %>% select(-one_of(drop.cols))
+shhs2.noadmin<-select(shhs2, -pm217, -pm227, -ecgdate)
+
+shhs1.noadmin<-select(shhs1, -ecgdate)
+
+cols_sq<-sq %>% select(id) %>% unlist(use.names = FALSE)
+
+assertthat::has_name(shhs1, cols_sq)
+
+#load function
+#look at data dict for rows to remove
+#verify cols are in there
+#remove cols from tibble
+
