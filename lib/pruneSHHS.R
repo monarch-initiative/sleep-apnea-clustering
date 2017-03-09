3b167be9aedfc63e59f77208148fbe4bf4633830
Jason Li <lja@ohsu.edu>
2017-03-09 10:28:30 -0800

    pruneSHHS uses lookUpFolderWithString

--- lib/pruneSHHS.R
+++ lib/pruneSHHS.R
@@ -5,17 +5,23 @@ pruneSHHS<-function (dataset, datadict){
   #Output: SHHS dataset w/ cols removed
   
   shhs.lower<-colsToLower(dataset)
-  df.admin<-datadict %>%
-    filter(str_detect(folder, "Administrative"))
+  df.admin <- lookUpFolderWithString(datadict, "Administrative")
+  #df.admin<-datadict %>%
+  #  filter(str_detect(folder, "Administrative"))
   terms_admin<-pullTerms(df.admin, shhs.lower)
   shhs.noadmin<-removeCols(shhs.lower, terms_admin)
   
-  df.sq<-datadict %>%
-    filter(str_detect(folder, "Signal Quality"))
+  df.sq <- lookUpFolderWithString(datadict, "Signal Quality")
   terms_nosq<-pullTerms(df.sq, shhs.noadmin)
   shhs.nosq<-removeCols(shhs.noadmin, terms_nosq)
   
-  drop.cols<-c("pm217", "pm227", "ecgdate")
+  df.specanal <- lookUpFolderWithString (datadict, "Spectral Analysis")
+  terms_nospecanal<-pullTerms(df.specanal, shhs.nosq)
+  shhs.nospecanal<-removeCols(shhs.nosq, terms_nospecanal)
+  
+  drop.cols<-c("pm217", "pm227", "ecgdate", "medalert", "lang15", "date10", 
+               "date25", "date02", "formdate_hi", "hi217", "formdate_ms", 
+               "formdate_ql", "formdate_sh")
   dataset.pruned<-removeCols(shhs.nosq, drop.cols)
   return (dataset.pruned)
 }
