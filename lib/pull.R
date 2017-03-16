219001af3e57bd0e705ee004c7c50d84913713bb
Jason Li <lja@ohsu.edu>
2017-03-15 18:50:53 -0700

    Delete pull.R

--- lib/pull.R
+++
@@ -1,5 +0,0 @@
-pull <- function(x,y) {
-  #pull out all values from column y in df/tibble x as a list of strings
-  #from stackexchange
-  x[,if(is.name(substitute(y))) deparse(substitute(y)) else y, drop = FALSE][[1]]
-}
