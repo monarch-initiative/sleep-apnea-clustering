b74320af21fca6ae417ead27651cd73d039fd640
Jason Li <lja@ohsu.edu>
2017-03-15 18:42:27 -0700

    look up string in display_name

---
+++ lib/lookUpDispname.R
@@ -0,0 +1,12 @@
+lookUpDispname <- function (ddict, string){
+  #This function will look at the column "folder" in ddict
+  #and will return a tibble of all rows which contain the string
+  #Dependencies: tidyverse, dplyr, stringr
+  #Input: ddict (data dictionary csv), string (any string)
+  #Output: df (tibble)
+  
+  df<-ddict %>%
+    filter(str_detect(display_name, string))
+  #put in error catching here
+  return(df)
+}
