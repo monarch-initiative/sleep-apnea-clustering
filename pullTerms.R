ed345b4819502203a925ea1b263e91dbf15c5be9
Jason Li <lja@ohsu.edu>
2017-03-15 18:44:11 -0700

    add error checking

---
+++ pullTerms.R
@@ -0,0 +1,21 @@
+pullTerms <-function(category_tibble, dataset){
+  
+  
+  #This function serves as a way to translate data dictionary row filters
+  #into shhs dataset column selections
+  #Requirements: tidyverse, dplyr
+  #Input: category_tibble is output of filter operation on data dictionary (rows
+  #fitting conditions, e.g. all rows w/Category="Administrative" and Description
+  #containing "Heart Rate"), dataset to be operated on (e.g. SHHS1, SHHS2, etc.)
+  #Output: list of strings corresponding to column names in dataset
+  terms_to_remove<-pull(category_tibble, id)
+  #Add error checking if cat_tibble doesn't exist
+  
+  pulled_cols<-filter(category_tibble, assertthat::has_name(dataset, terms_to_remove)) %>%
+    select (id) %>% 
+    collect %>% 
+    .[["id"]]
+  #assertthat::has_name makes sure terms_to_remove are actually in dataset
+  
+  return (pulled_cols)
+}
