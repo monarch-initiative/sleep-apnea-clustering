bd786aef8c6db20299d9c793919852818170a13c
Jason Li <lja@ohsu.edu>
2017-03-20 17:28:14 -0700

    Delete removeCols.R

--- lib/removeCols.R
+++
@@ -1,9 +0,0 @@
-removeCols<-function(dataset, cols_to_remove){
-  #removes selected columns in given dataset
-  #Requirements: dplyr, tidyverse
-  #Input: dataset is tibble, cols_to_remove is list of strings (column names)
-  #Output: dataset.cols.removed (tibble with columns removed)
-  dataset.cols.removed<-dataset %>%
-    select(-one_of(cols_to_remove))
-  return (dataset.cols.removed)
-}
