9fe48e599d5e538a60cc852e8c0648beb638bb32
Jason Li <lja@ohsu.edu>
2017-03-20 17:26:47 -0700

    Delete basicSubset.R

--- basicSubset.R
+++
@@ -1,7 +0,0 @@
-basicSubset<-function(datadict, dataset, string){
-  df <- lookUpFolder(datadict, string)
-  terms<-pullTerms(df, dataset)
-  terms<-c("obf_pptid", terms)
-  sub_data<-subsetCols(dataset, terms)
-  return(sub_data)
-}
