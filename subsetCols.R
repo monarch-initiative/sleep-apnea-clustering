eaef8cf1604f52f5505313459a6958254f18a58e
Jason Li <lja@ohsu.edu>
2017-03-20 17:24:25 -0700

    now can take col names not in original frame

---
+++ subsetCols.R
@@ -0,0 +1,12 @@
+subsetCols<-function(dataset, cols_to_subset){
+  #Same as removeCols, but selecting FOR columns named in cols_to_subset
+  #Requirements: dplyr, tidyverse
+  #Input: dataset is tibble, cols_to_subset is list of strings (column names)
+  #Output: dataset.cols.subsetted (tibble with only selected columns)
+  
+  term_isthere<-has_name(dataset, cols_to_subset)
+  dataset.cols.subsetted<-dataset %>%
+    dplyr::select(one_of(cols_to_subset[term_isthere]))
+  
+  return(dataset.cols.subsetted)
+}
