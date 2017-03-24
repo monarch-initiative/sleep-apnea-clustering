cdb1279180009da5103e72d3d728ac4fb4c33e72
Jason Li <lja@ohsu.edu>
2017-03-23 21:45:12 -0700

    Cramer's V test

---
+++ lib/cvtest.R
@@ -0,0 +1,6 @@
+cv.test = function(x,y) {
+  CV = sqrt(chisq.test(x, y, correct=FALSE)$statistic /
+              (length(x) * (min(length(unique(x)),length(unique(y))) - 1)))
+  print.noquote("Cramér V / Phi:")
+  return(as.numeric(CV))
+}
