179fb0a58a2102f0f20e8895f4268ca7a284041e
Jason Li <lja@ohsu.edu>
2017-03-23 21:48:04 -0700

    added error check

--- lib/subsetCols.R
+++ lib/subsetCols.R
@@ -4,6 +4,9 @@ subsetCols<-function(dataset, cols_to_subset){
   #Input: dataset is tibble, cols_to_subset is list of strings (column names)
   #Output: dataset.cols.subsetted (tibble with only selected columns)
   
+  #Error
+  #wrong name for dataset, wrong name for cols_to_subset
+  #
   term_isthere<-has_name(dataset, cols_to_subset)
   dataset.cols.subsetted<-dataset %>%
     dplyr::select(one_of(cols_to_subset[term_isthere]))
