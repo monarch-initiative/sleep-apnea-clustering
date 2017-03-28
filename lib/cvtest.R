1351841fff214a3ffe0204fe300051c81970b8f2
Jason Li <lja@ohsu.edu>
2017-03-28 10:27:09 -0700

    Cramer's V function

--- lib/cvtest.R
+++ lib/cvtest.R
@@ -1,6 +1,8 @@
 cv.test = function(x,y) {
   CV = sqrt(chisq.test(x, y, correct=FALSE)$statistic /
               (length(x) * (min(length(unique(x)),length(unique(y))) - 1)))
+  #p = chisq.test(x, y, correct=FALSE)$p.value
   print.noquote("Cramér V / Phi:")
+  #CVlist=c(as.numeric(CV), as.numeric(p))
   return(as.numeric(CV))
 }
