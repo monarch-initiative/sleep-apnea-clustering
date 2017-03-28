9d21eb6a4db0c543d0988499750bf70cb2e5d085
Jason Li <lja@ohsu.edu>
2017-03-28 10:30:37 -0700

    now returns plot p to main

--- lib/plotMeltedCorr.R
+++ lib/plotMeltedCorr.R
@@ -1,8 +1,10 @@
+require(reshape2)
+
 plotMeltedCorr<-function (correlated) {
   melted<-melt(correlated)
-  qplot(x=Var1, y=Var2, data=melted, fill=value, geom="tile") +
+  p <- qplot(x=Var1, y=Var2, data=melted, fill=value, geom="tile") +
     scale_fill_gradient2(limits=c(-1, 1)) +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   
-  return(melted)
+  return(p)
 }
