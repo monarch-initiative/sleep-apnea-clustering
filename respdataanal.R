3872bbe4e3b50b95aa1a8f77b127279157560234
Jason Li <lja@ohsu.edu>
2017-03-28 10:23:40 -0700

    generates all graphs for resp

--- respdataanal.R
+++ respdataanal.R
@@ -5,37 +5,48 @@
 # Hypopnea
 
 df.oxysat <- basicSubset(datadict, shhs1.pruned, "Oxygen Saturation")
+df.centap <-basicSubset(datadict, shhs1.pruned, "Central")
+df.obap <-basicSubset(datadict, shhs1.pruned, "Obstructive")
+df.hypop <-basicSubset(datadict, shhs1.pruned, "Hypopnea")
+df.hr <- basicSubset(datadict, shhs1.pruned, "Heart Rate")
+df.arousal <-basicSubset(datadict, shhs1.pruned, "Arousals")
+df.index <-basicSubset(datadict, shhs1.pruned, "Indexes")
 
+shhs1.avg.noarous %>%
+  select(-obf_pptid) %>%
+  summary
+
+df<-df.oxysat
 #remove min and max
 df.min<-lookUpDispname(datadict, "Minimum")
 df.max<-lookUpDispname(datadict, "Maximum")
-terms_min<-pullTerms(df.min, df.oxysat)
-terms_max<-pullTerms(df.max, df.oxysat)
+terms_min<-pullTerms(df.min, df)
+terms_max<-pullTerms(df.max, df)
 terms_minmax<-c(terms_min, terms_max)
 
 #w/ w/o arousals
 df.arous <- lookUpDispname(datadict, "w/ arousals")
-terms_arous<-pullTerms(df.arous, df.oxysat)
+terms_arous<-pullTerms(df.arous, df)
 
 #NREM REM
 df.nrem <- lookUpDispname(datadict, "NREM")
-terms_nrem<-pullTerms(df.nrem, df.oxysat)
+terms_nrem<-pullTerms(df.nrem, df)
 
 #Back Other
 df.back <- lookUpDispname(datadict, "Back")
-terms_back <- pullTerms(df.back, df.oxysat)
+terms_back <- pullTerms(df.back, df)
 
 #all 2 3 4 5%
 df.2p<-lookUpDispname(datadict, "2%")
-terms_2p<-pullTerms(df.2p, df.oxysat)
+terms_2p<-pullTerms(df.2p, df)
 df.3p<-lookUpDispname(datadict, "3%")
-terms_3p<-pullTerms(df.3p, df.oxysat)
+terms_3p<-pullTerms(df.3p, df)
 df.4p<-lookUpDispname(datadict, "4%")
-terms_4p<-pullTerms(df.4p, df.oxysat)
+terms_4p<-pullTerms(df.4p, df)
 df.5p<-lookUpDispname(datadict, "5%")
-terms_5p<-pullTerms(df.5p, df.oxysat)
+terms_5p<-pullTerms(df.5p, df)
 
-shhs1.avg<-removeCols(df.oxysat, terms_minmax)
+shhs1.avg<-removeCols(df, terms_minmax)
 shhs1.avg.noarous<-removeCols(shhs1.avg, terms_arous)
 shhs1.avg.noarous.2p<-subsetCols(shhs1.avg.noarous, terms_2p)
 shhs1.avg.noarous.5p<-subsetCols(shhs1.avg.noarous, terms_5p)
@@ -43,17 +54,19 @@ shhs1.avg.noarous.5p<-subsetCols(shhs1.avg.noarous, terms_5p)
 shhs1.avg.noarous.back<-subsetCols(shhs1.avg.noarous, terms_back)
 shhs1.avg.noarous.other<-removeCols(shhs1.avg.noarous, terms_back)
 
+shhs1.avg.noarous.nrem<-subsetCols(shhs1.avg.noarous, terms_nrem)
+
 # reorganize data
-long.df <- shhs1.noarous.avg.nrem %>% 
-  select(-avsao2nh) %>%
-  rename(avdnbpall=avdnbp, avdnopall=avdnop) %>%
+long.df <- shhs1.avg.noarous.nrem %>% 
+  select(starts_with("av")) %>%
+  rename(avhnbp0=avhnbp, avhnop0=avhnop) %>%
   gather(col_name, value) %>%
-  separate(col_name, into=c("position", "percent"), sep=6) %>%
+  separate(col_name, into=c("position", "percent"), sep=5) %>%
   mutate(position = as.factor(position), percent=as.factor(percent))
 
-long.df <- shhs1.avg.noarous.other %>% 
-  select(starts_with("avd")) %>%
-  rename(avdnop0=avdnop, avdrop0=avdrop) %>%
+long.df <- shhs1.avg.noarous.back %>% 
+  select(starts_with("av")) %>%
+  rename(avhnbp0=avhnbp, avhrbp0=avhrbp) %>%
   gather(col_name, value) %>%
   separate(col_name, into=c("stage", "percent"), sep=4) %>%
   mutate(stage = as.factor(stage), percent=as.factor(percent))
@@ -64,6 +77,20 @@ long.df <- shhs1.avg.noarous.5p %>%
   separate(col_name, into=c("stage", "position"), sep=4) %>%
   mutate(stage=as.factor(stage), position=as.factor(position))
 
+
+  
+long.df <- shhs1.avg.noarous.2p %>%
+  select(starts_with("av")) %>% #select(-ndes5ph) %>%
+  gather(col_name, value) %>%
+  separate(col_name, into=c("stage", "position"), sep=4, remove=TRUE) %>%
+  mutate(stage=as.factor(stage), position=as.factor(position))
+
+shhs1.avg.noarous.2p %>% 
+  select(-starts_with("av")) %>%
+  na.omit %>%
+  #summary
+  boxplot
+
 boxplot(shhs1.avg.noarous.other %>%
   select(starts_with("ndes")))
 
@@ -71,12 +98,19 @@ boxplot(shhs1.avg.noarous.other %>%
   select(contains("sao2")) %>%
   mutate_each(funs(log)))
 
+boxplot(shhs1.avg.noarous.nrem %>%
+          select(-starts_with("av")))
 #boxplot, scatterplot
+ggplot(long.df, aes(y=log(value), x=stage, fill=position))+geom_boxplot()
 ggplot(long.df, aes(y=log(value), x=stage, fill=percent))+geom_boxplot()
+ggplot(long.df, aes(y=log(value), x=position, fill=percent))+geom_boxplot()
+
 ggplot(shhs1.avg.noarous.5p, aes(y=avdnbp5, x=avdrop5)) +
   geom_point(shape=1) +    # Use hollow circles
   geom_smooth(method=lm,   # Add linear regression line
               se=FALSE)
+
+
 
 #correlation
 cor(na.omit(shhs1.avg.noarous.5p))
@@ -130,3 +164,95 @@ DF[] <- lapply(DF,as.integer)
 library(sjPlot)
 sjp.corr(DF)
 sjt.corr(DF)
+
+long.df <- shhs1.avg %>% 
+  select(-obf_pptid) %>%
+  #rename(avhnbp0=avhnbp, avhnop0=avhnop) %>%
+  gather(col_name, value) %>%
+  separate(col_name, into=c("stage", "position"), sep=5) %>%
+  mutate(stage= as.factor(stage), position=as.factor(position))
+
+ggplot(data=long.df, aes(y=value, x=stage, fill=position)) + geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+long.df<-df.index %>%
+  select(starts_with("rdi"), -obf_pptid, -ends_with("a")) %>%
+  gather(col_name, value)
+
+ggplot(data=long.df, aes(y=value, x=col_name)) + geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+df.index %>%
+  select(starts_with("ahi"), -obf_pptid) %>%
+  boxplot
+
+df.index %>%
+  select(starts_with("c")) %>% boxplot
+  gather(col_name, value)
+  
+df.resp <- df.index %>%
+  left_join(df.oxysat) %>%
+  left_join(df.centap) %>%
+  left_join(df.obap) %>%
+  left_join(df.hypop) %>%
+  left_join(df.hr) %>%
+  left_join(df.arousal)
+
+resp_corr<-corrNASpear(df.oxysat %>% select (-obf_pptid))
+melted<-melt(resp_corr)
+qplot(x=Var1, y=Var2, data=melted, fill=value, geom="tile") +
+  scale_fill_gradient2(limits=c(-1, 1)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+#find most correlated features
+# cutoff  abs .75?
+#which one has more missing values?
+#which one has less variability?
+# eliminate redundant feat w more missing values and less variance
+
+melteddesc<-melted %>%
+  arrange(desc(value)) %>%
+  filter(abs(value)>.90) %>%
+  filter(Var1 != Var2)
+
+#oahi
+#rdi3p
+terms_to_remove<-unique(pull(melteddesc, Var1))
+terms_to_remove1<-sapply(terms_to_remove, as.character)
+correlated<-df.oxysat %>%
+  select(one_of(terms_to_remove1))
+
+
+
+
+na_count <- function (y) {
+  z<-sum(length(which(is.na(y))))
+  return(z)
+}
+na_count(correlated[,1])
+nas<-sapply(correlated, na_count)
+data.frame(nas)
+
+
+correlated.nona<-removeCols(correlated, names(which(nas>0)))
+
+corrdf<-sapply(correlated.nona, IQR, na.rm=TRUE)
+
+rangedf<-sapply(correlated.nona, range, na.rm=TRUE)
+variation<-corrdf/(rangedf[2, ]-rangedf[1, ])
+
+keeplist<-c(names(which.max(variation)), 
+            names(which.max(rangedf[2,]-rangedf[1,])), 
+            names(which.max(corrdf)))
+setdiff(terms_to_remove1, keeplist)
+removeCols(df.oxysat, setdiff(terms_to_remove1, keeplist))
+
+df.oxysat.nocor<-removeCorrelated(df.oxysat)
+df.index.nocor<-removeCorrelated(df.index)
+
+mat<-left_join(df.oxysat.nocor, df.index.nocor)
+resp_corr<-corrNASpear(mat)
+melted<-melt(resp_corr)
+qplot(x=Var1, y=Var2, data=melted, fill=value, geom="tile") +
+  scale_fill_gradient2(limits=c(-1, 1)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
