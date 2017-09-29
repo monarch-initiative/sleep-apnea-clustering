d11f3caff0298acc7cb501ecbb4e295132fdb054
Jason Li <lja@ohsu.edu>
2017-09-29 11:26:41 -0700

    added comments, other datasets

--- shhs-process.R
+++ shhs-process.R
@@ -1,7 +1,10 @@
+#main function to load data and initially process before cleaning
+
 library(tidyverse)
 library(stringr)
 
-setwd("~/Documents/MSthesis/gitdir")
+setwd("~/Documents/MSthesis/gitdir") #change this line to location of cloned repo
+
 source("lib/pull.R")
 source("lib/colsToLower.R")
 source("lib/removeCols.R")
@@ -13,16 +16,29 @@ source("lib/lookUpDispname.R")
 
 
 #load data
+#make sure you have all these files (or comment out files you don't have)
+#these will be from NSRR
 
 shhs1<-read_csv("datasets/shhs1-dataset-0.11.0.csv")
 shhs2<-read_csv("datasets/shhs2-dataset-0.11.0.csv")
+cvdevents<-read_csv("datasets/shhs-cvd-events-dataset-0.11.0.csv")
+cvdsum<-read_csv("datasets/shhs-cvd-summary-dataset-0.11.0.csv")
+shhs1.hrv<-read_csv("datasets/shhs1-hrv-summary-0.11.0.csv")
+shhs2.hrv<-read_csv("datasets/shhs2-hrv-summary-0.11.0.csv")
+interim<-read_csv("datasets/shhs-interim-followup-dataset-0.11.0.csv")
 
+#data dictionary, make sure you have this file
 datadict<-read_csv("datasets/shhs-data-dictionary-0.11.0-variables.csv")
+
 
 #process
 
 shhs1.pruned<-pruneSHHS(shhs1, datadict)
 shhs2.pruned<-pruneSHHS(shhs2, datadict)
+cvd.pruned<-pruneSHHS(cvdsum, datadict)
+shhs1.hrv.pruned<-pruneSHHS(shhs1.hrv, datadict)
+shhs2.hrv.pruned<-pruneSHHS(shhs2.hrv, datadict)
+interim.pruned<-pruneSHHS(interim, datadict)
 
 
 
